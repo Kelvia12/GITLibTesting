5cf10c71ee1b25f1b11e1614200c26b487daac1d
kelvin luvila <kelvin.luvila@elections.org.zm>
2019-12-24 16:41:11 +0200

    Add project files.

---
+++ GITLibTesting/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GITLibTesting
+{
+    public class Class1
+    {
+        public void print()
+    }
+}
---
+++ GITLibTestingTests/Class1Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GITLibTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GITLibTesting.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void printTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
