310da43677817cb291d42a2bd8f0b2062ba40f22
ndean73 <Dean.Nalumpa@mlnr.gov.zm>
2019-12-26 02:18:21 -0800

    Task: Added Printer class

--- GITLibTesting/Class1.cs
+++ GITLibTesting/Class1.cs
@@ -4,6 +4,16 @@ namespace GITLibTesting
 {
     public class Class1
     {
+        Printer p = new Printer();
+
+        // private String testmessage = "This is a test print";
+        private readonly String testmessage = p.PrintTest("This is a test print");
         public void print()
+        {
+
+
+            Console.WriteLine(testmessage);
+
+        }
     }
 }
